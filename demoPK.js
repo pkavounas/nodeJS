0915a7489e434e14d3f0f76c9477de4a7ef8fb04
Peter Kavounas <PKavounas@eastsideprep.org>
2019-06-04 16:47:02 -0700

    added upload button

--- demoPK.js
+++ demoPK.js
@@ -1,26 +1,13 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-var fs = require("fs");
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('The date today is: '+ date() +'\n')
-
-  fs.readFile("temp.txt", function(err, data) {
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  //Open a file on the server and return its content:
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
+    
+    return res.end();
   });
-  
-  res.end('Hello World\n');
-});
+}).listen(8080);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-
- function date(){
-    return Date();
-  }
+console.log("Server Running on 8080")
